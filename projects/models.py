759946f9d4453da25f72844ccdbdb12fd682157a
Titus Ouko <titusouko@gmail.com>
2019-11-24 09:53:29 +0300

    Add class profile

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
 
-# Create your models here.
+Class Profile(models.Model):
+user= models.OneToOneField(User, on_delete=models.CASCADE)
+profile_picture =models.ImageField(upload_to= 'profiles/', blank=True, default="profiles/a.jpg")
+bio = models.CharField(max_length=100, default='Welcome to you bio')
+contact = models.CharField(max_length=80)
