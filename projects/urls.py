9a5aae247c972aa0d3ad3376eda9ee9eb244cfe5
Titus Ouko <titusouko@gmail.com>
2019-11-24 15:46:55 +0300

    Add urls for api

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,14 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^update/profile$', views.updateprofile, name='update'),
+    url(r'^profile/(?P<user_id>\d+)?$', views.profile, name='profile'),
+    url(r'^new/post$', views.new_post, name='new_post'),
+    url(r'^vote/(?P<post_id>\d+)?$', views.vote, name='vote'), 
+     url(r'^search/', views.search_results, name='search_results'),
+    url(r'^api/post/$', views.PostList.as_view()),
+    url(r'^api/profile/$', views.ProfileList.as_view()),
 ]
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
